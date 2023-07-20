6b6d4a7e37749fd5a9f3fa820c850527efdd0e2b
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-07-20 15:48:17 +0600

    * installation: firebase, react-router-dom * external setup: Added configuration details from firebase into ./firebaseConfig.js * boilerplate cleanup: Removed App.jsx, App.css & redundant styles from index.css * custom setup: Under ./src created -- /components/LoginComponent.jsx, /pages/Login.jsx, /routes/index.jsx * modification: Created main.jsx as the root of the application using RouterProvider * caution: default route '/' is no longer accessible, '/login' is the only working route

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCN_zwz7Cox8YYhZdCtXdaeFHx6X3Wz1gE",
+    authDomain: "linkedin-clone-48f59.firebaseapp.com",
+    projectId: "linkedin-clone-48f59",
+    storageBucket: "linkedin-clone-48f59.appspot.com",
+    messagingSenderId: "927675666778",
+    appId: "1:927675666778:web:a6cc37dd68e0c4a792a363",
+    measurementId: "G-31WMYTZ1PK",
+};
+
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
